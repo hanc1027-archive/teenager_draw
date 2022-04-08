7bddca08e4cab248a6d2a01c88eae055296db281
Nelson Hsu <nelsonhsu1027@gmail.com>
2022-04-09 01:28:31 +0800

    Add members_list.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-container fluid >
+    <v-row justify-center>
+       <v-col
+        class="d-flex"
+        cols="8"
+        md="3"
+      >
+      <h3>請選擇每一小隊欲中選人數</h3>
+      </v-col>
+    </v-row>
+    <v-row justify-center>
+     <v-col
+        class="d-flex"
+        cols="8"
+        sm="5"
+        md="3"
+      >
+        <v-select
+          :items="items"
+          label="每小隊人數"
+          outlined
+        ></v-select>
+      </v-col>
+      </v-row>
+     <v-row justify-center>
+        <v-col
+        class="d-flex"
+        cols="8"
+        md="3"
+      >
+      <v-btn @click="draw">抽籤</v-btn>
+      </v-col>
+      </v-row>
+    </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data(){
+    return{
+      items:[1,2,3,4,5,6]
+    }
   }
 }
 </script>
